8471c61e32ebed68c20c587f656735943990b9f8
Cho Je-seung <tysc1412@naver.com>
2025-09-23 21:48:49 +0900

    프로젝트 셋업

---
+++ src/test/java/com/zerobase/homemate/HomemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.homemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
