45bb2dae1094c439eb76077a82e553dcca1e4dc8
hj <kwonfelix93@gmail.com>
2025-09-25 13:57:35 +0900

    [refactor] 유효성 검증 예외 처리 시 getFieldErrors() 와 stream().map() 사용

--- src/main/java/com/zerobase/homemate/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/homemate/exception/GlobalExceptionHandler.java
@@ -3,12 +3,10 @@ package com.zerobase.homemate.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -30,18 +28,33 @@ public class GlobalExceptionHandler {
 
     // 유효성 검증 예외 처리 (@Valid)
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException2(MethodArgumentNotValidException e) {
+        log.error("유효성 검증 실패: {}", e.getMessage());
+
+        List<ErrorResponse.ValidationError> details =
+            e.getBindingResult().getFieldErrors().stream().map(
+                fieldError -> ErrorResponse.ValidationError.builder().field(
+                    fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build()
+            ).toList();
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.VALIDATION_ERROR, details);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    // 유효성 검증 예외 처리 (@Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
         log.error("유효성 검증 실패: {}", e.getMessage());
         
-        List<ErrorResponse.ValidationError> details = new ArrayList<>();
-        e.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            details.add(ErrorResponse.ValidationError.builder()
-                    .field(fieldName)
-                    .message(errorMessage)
-                    .build());
-        });
+        List<ErrorResponse.ValidationError> details =
+            e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> ErrorResponse.ValidationError.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build())
+                    .toList();
         
         ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.VALIDATION_ERROR, details);
         
