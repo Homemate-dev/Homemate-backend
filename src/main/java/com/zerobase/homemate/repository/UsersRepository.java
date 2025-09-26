61bddb3a4553c9ccb95852386da977330183d042
Cho Je-seung <tysc1412@naver.com>
2025-09-26 20:21:37 +0900

    feat: Users 엔티티/레포지토리, 관련 enum 추가

---
+++ src/main/java/com/zerobase/homemate/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.zerobase.homemate.repository;
+
+import com.zerobase.homemate.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+}
