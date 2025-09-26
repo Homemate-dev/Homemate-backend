19395a992b09a17884a20e8e1bf19d1ac68906b1
Cho Je-seung <tysc1412@naver.com>
2025-09-26 20:24:12 +0900

    feat: Security 설정 추가

---
+++ src/main/java/com/zerobase/homemate/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.zerobase.homemate.config;
+
+import com.zerobase.homemate.auth.security.JwtAuthenticationFilter;
+import com.zerobase.homemate.auth.service.JwtService;
+import com.zerobase.homemate.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final JwtService jwtService;
+  private final UsersRepository usersRepository;
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    JwtAuthenticationFilter jwtFilter = new JwtAuthenticationFilter(jwtService, usersRepository);
+
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/auth/**", "/policies/**").permitAll()
+            .anyRequest().authenticated()
+        )
+//        .exceptionHandling() 예외처리 추가 예정
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+}
