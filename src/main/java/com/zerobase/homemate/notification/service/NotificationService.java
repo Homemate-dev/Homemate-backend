6777761539cd2c38bd3a7e3173f9cd3d610c3406
escha-k <bbs7380@gmail.com>
2025-09-27 23:30:24 +0900

    feat: 알림 읽음 처리 서비스 구현

--- src/main/java/com/zerobase/homemate/notification/service/NotificationService.java
+++ src/main/java/com/zerobase/homemate/notification/service/NotificationService.java
@@ -2,13 +2,17 @@ package com.zerobase.homemate.notification.service;
 
 import com.zerobase.homemate.entity.Notification;
 import com.zerobase.homemate.entity.enums.NotificationCategory;
+import com.zerobase.homemate.exception.CustomException;
 import com.zerobase.homemate.notification.dto.NotificationDto;
+import com.zerobase.homemate.notification.dto.NotificationReadDto;
 import com.zerobase.homemate.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.zerobase.homemate.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +32,19 @@ public class NotificationService {
         List<Notification> list = notificationRepository.findAllByUserIdAndNotificationCategory(userId, category);
 
         return list.stream().map(NotificationDto::fromEntity).toList();
+    }
+
+    @Transactional
+    public NotificationReadDto updateNotificationToRead(Long userId, Long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new CustomException(NOTIFICATION_NOT_FOUND));
+
+        if (!userId.equals(notification.getUserId())) {
+            throw new CustomException(FORBIDDEN, "해당 알림을 수정할 권한이 없습니다.");
+        }
+
+        notification.read();
+
+        return NotificationReadDto.from(notification);
     }
 }
