79f4869ea87fcb0a5dd8a63d419d1f82f2922007
Delcastin <lejesu96@naver.com>
2025-09-30 00:34:00 +0900

    📝 계획 💡PR 예정 핵심적으로 변경된 사항 - Category는 기획상에서 확인된 대로 정해진 값이 들어간 점이 확인되었습니다. 이에 Enum으로 설계를 선회하였습니다. - 필수 Service, Repository, Controller 계층을 작성하였습니다. - 일부 CategoryRepository 폐기, Frequency Enum 폐기, 일단은 기능 하나하나에만 집중하고자 카테고리별로 조회하는 기능과 관련 없는 파일들 모두 폐기하였습니다.
    - 리스트를 조회할 때에는 와이어프레임을 참고하여 한 page당 최대 4개씩 조회가 가능하게끔 작성하였습니다. -> 스크롤 기능 폐기
    
    📢이외 추가 변경 부분 및 기타
    테스트 코드를 작성할 예정,
    updateName 메소드(카테고리 이름 수정 목적),
    
    ✅ 테스트
     테스트 코드
     API 테스트

---
+++ src/test/java/com/zerobase/homemate/recommend/CategoryControllerTest.java
@@ -0,0 +1,28 @@
+package com.zerobase.homemate.recommend;
+
+
+import com.zerobase.homemate.recommend.controller.CategoryController;
+import com.zerobase.homemate.recommend.service.CategoryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+
+@WebMvcTest(CategoryController.class)
+class CategoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CategoryService categoryService;
+
+
+
+
+}
