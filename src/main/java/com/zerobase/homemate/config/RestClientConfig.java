d5116e6feee35136dca3b95f46292bb0c49e07f2
Cho Je-seung <tysc1412@naver.com>
2025-10-01 19:25:31 +0900

    chore: 메인에 @ConfigurationPropertiesScan 적용, RestClientConfig의 Enable 제거

--- src/main/java/com/zerobase/homemate/config/RestClientConfig.java
+++ src/main/java/com/zerobase/homemate/config/RestClientConfig.java
@@ -1,15 +1,12 @@
 package com.zerobase.homemate.config;
 
-import com.zerobase.homemate.auth.kakao.KakaoOAuthProperty;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@EnableConfigurationProperties(KakaoOAuthProperty.class)
 public class RestClientConfig {
   @Value("${http.client.connect-timeout-ms:3000}")
   private int connectTimeout;
