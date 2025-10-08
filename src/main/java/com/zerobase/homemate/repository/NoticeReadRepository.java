7695975d13413c6c03b27bcd97c8370af0f3d6cc
escha-k <bbs7380@gmail.com>
2025-10-08 21:28:33 +0900

    feat: NoticeRead 리포지토리 생성

---
+++ src/main/java/com/zerobase/homemate/repository/NoticeReadRepository.java
@@ -0,0 +1,7 @@
+package com.zerobase.homemate.repository;
+
+import com.zerobase.homemate.entity.NoticeRead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeReadRepository extends JpaRepository<NoticeRead, Long> {
+}
