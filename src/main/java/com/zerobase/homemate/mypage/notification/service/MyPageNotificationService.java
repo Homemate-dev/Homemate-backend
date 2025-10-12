75ff9f4fdf0d657c28f089d5467e547c56618d62
Cho Je-seung <tysc1412@naver.com>
2025-10-12 16:41:51 +0900

    fix: updatedAt 즉시 반영을 위한 saveAndFlush 추가

--- src/main/java/com/zerobase/homemate/mypage/notification/service/MyPageNotificationService.java
+++ src/main/java/com/zerobase/homemate/mypage/notification/service/MyPageNotificationService.java
@@ -37,6 +37,7 @@ public class MyPageNotificationService {
     }
 
     setting.completeFirstSetup(time);
+    userNotificationSettingRepository.saveAndFlush(setting);
 
     return FirstSetupResponse.from(setting);
   }
