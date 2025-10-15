2c353cccee192c3906fd6de2980006a46dd7e2ab
Cho Je-seung <tysc1412@naver.com>
2025-10-16 04:47:57 +0900

    feat: 알림 토글 API 단일화 및 경로 변경

--- src/main/java/com/zerobase/homemate/mypage/notification/controller/MyPageNotificationController.java
+++ src/main/java/com/zerobase/homemate/mypage/notification/controller/MyPageNotificationController.java
@@ -4,8 +4,8 @@ import com.zerobase.homemate.auth.security.UserPrincipal;
 import com.zerobase.homemate.mypage.notification.dto.FirstSetupStatusDto.FirstSetupRequest;
 import com.zerobase.homemate.mypage.notification.dto.FirstSetupStatusDto.FirstSetupResponse;
 import com.zerobase.homemate.mypage.notification.dto.FirstSetupStatusDto.FirstSetupStatusResponse;
-import com.zerobase.homemate.mypage.notification.dto.NotificationSettingDto.MasterToggleRequest;
-import com.zerobase.homemate.mypage.notification.dto.NotificationSettingDto.MasterToggleResponse;
+import com.zerobase.homemate.mypage.notification.dto.NotificationSettingDto;
+import com.zerobase.homemate.mypage.notification.dto.NotificationSettingDto.ToggleResponse;
 import com.zerobase.homemate.mypage.notification.dto.NotificationTimeDto.NotiTimeRequest;
 import com.zerobase.homemate.mypage.notification.dto.NotificationTimeDto.NotiTimeResponse;
 import com.zerobase.homemate.mypage.notification.service.MyPageNotificationService;
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,11 +55,13 @@ public class MyPageNotificationController {
         myPageNotificationService.updateNotificationTime(user.id(), request.notificationTime()));
   }
 
-  @PatchMapping("/master")
-  public ResponseEntity<MasterToggleResponse> toggleMaster(
+  @PatchMapping("/{type}")
+  public ResponseEntity<ToggleResponse> toggle(
       @AuthenticationPrincipal UserPrincipal user,
-      @Valid @RequestBody MasterToggleRequest request) {
+      @PathVariable String type,
+      @Valid @RequestBody NotificationSettingDto.ToggleRequest request) {
 
-    return ResponseEntity.ok(myPageNotificationService.toggleMaster(user.id(), request.enabled()));
+    return ResponseEntity.ok(
+        myPageNotificationService.toggleNotification(user.id(), type, request.enabled()));
   }
 }
