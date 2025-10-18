07a028485b4fc585da20bad627f55e0d38a9272c
escha-k <bbs7380@gmail.com>
2025-10-18 21:38:31 +0900

    feat: 이벤트 리스너 구현

---
+++ src/main/java/com/zerobase/homemate/push/component/ChoreInstanceEventListener.java
@@ -0,0 +1,29 @@
+package com.zerobase.homemate.push.component;
+
+import com.zerobase.homemate.push.service.ChoreNotificationSchedulerService;
+import lombok.RequiredArgsConstructor;
+import org.quartz.SchedulerException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class ChoreInstanceEventListener {
+
+    private final ChoreNotificationSchedulerService schedulerService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleCreated(ChoreInstanceCreatedEvent event) {
+        try {
+            schedulerService.scheduleChoreNotification(event);
+        } catch (SchedulerException e) {
+            // TODO: 예외 처리 구성
+        }
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleCancelled(ChoreInstanceCancelledEvent event) {
+        // TODO: ChoreInstance 취소 시 로직 구성
+    }
+}
