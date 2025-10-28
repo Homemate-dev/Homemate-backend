1837f4bedb59ab73983b3d761743115fd06cdc59
escha-k <bbs7380@gmail.com>
2025-10-28 18:56:50 +0900

    feat: 공지 알림을 위한 fcm 푸시 전체 발송

--- src/main/java/com/zerobase/homemate/notification/push/service/FcmPushService.java
+++ src/main/java/com/zerobase/homemate/notification/push/service/FcmPushService.java
@@ -5,17 +5,23 @@ import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import com.zerobase.homemate.entity.FcmToken;
 import com.zerobase.homemate.entity.User;
+import com.zerobase.homemate.notification.push.dto.TokenWithIdDto;
 import com.zerobase.homemate.repository.FcmTokenRepository;
+import com.zerobase.homemate.repository.UserNotificationSettingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class FcmPushService {
+
+    public static final int DB_BATCH_SIZE = 2000;
+    public static final int FCM_BATCH_SIZE = 500;
 
     private final FcmTokenRepository fcmTokenRepository;
 
@@ -29,6 +35,34 @@ public class FcmPushService {
         }
 
         sendBatch(tokens, title, message);
+    }
+
+    public void sendGlobal(String title, String message) {
+        long lastId = 0L;
+
+        while (true) {
+            List<TokenWithIdDto> list = fcmTokenRepository.findIdAndTokenBatch(lastId, DB_BATCH_SIZE);
+
+            List<String> sendBuffer = new ArrayList<>(FCM_BATCH_SIZE);
+            for (TokenWithIdDto t : list) {
+                sendBuffer.add(t.token());
+
+                if (sendBuffer.size() >= FCM_BATCH_SIZE) {
+                    sendBatch(sendBuffer, title, message);
+                    sendBuffer.clear();
+                }
+
+                lastId = t.id();
+            }
+
+            if (!sendBuffer.isEmpty()) {
+                sendBatch(sendBuffer, title, message);
+            }
+
+            if (list.size() < DB_BATCH_SIZE) {
+                break;
+            }
+        }
     }
 
     public void sendBatch(List<String> tokens, String title, String message) {
