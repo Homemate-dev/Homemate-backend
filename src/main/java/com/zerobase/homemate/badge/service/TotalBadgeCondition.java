745937bf8b6da4df03b405d12e6f304df3bd1da8
Delcastin <lejesu96@naver.com>
2025-11-23 09:42:15 +0900

    [fix] badge_service
    BadgeService & UserBadgeStatsService 로직 개선
    
    - 단위 테스트 완료
    
    To do
    - 시나리오 - 통합 테스트 작성 완료
    - Refactoring - 완료
    -> UserActionType.COMPLETE_CHORE_WITH_SPACE 미션 탐구 완료
    - API 출력 확인 이상 없음.

---
+++ src/main/java/com/zerobase/homemate/badge/service/TotalBadgeCondition.java
@@ -0,0 +1,22 @@
+package com.zerobase.homemate.badge.service;
+
+import com.zerobase.homemate.entity.Chore;
+import com.zerobase.homemate.entity.User;
+
+public class TotalBadgeCondition implements BadgeCondition {
+
+    private final int requiredCount;
+    private final UserBadgeStatsService userBadgeStatsService;
+
+    public TotalBadgeCondition(int requiredCount, UserBadgeStatsService userBadgeStatsService) {
+        this.requiredCount = requiredCount;
+        this.userBadgeStatsService = userBadgeStatsService;
+    }
+
+    @Override
+    public boolean matchesCondition(User user, Chore chore) {
+        Long userId = chore.getUser().getId();
+        long completedCount = userBadgeStatsService.getTotalCompletedCount(userId);
+        return completedCount >= requiredCount;
+    }
+}
