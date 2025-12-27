2c0e7833323055688b7baacc9b82eee15ed943d6
escha-k <bbs7380@gmail.com>
2025-12-27 18:21:16 +0900

    test: RefreshTokenFactory 테스트 작성

---
+++ src/test/java/com/zerobase/homemate/auth/support/RefreshTokenFactoryTest.java
@@ -0,0 +1,32 @@
+package com.zerobase.homemate.auth.support;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseCookie;
+
+import static com.zerobase.homemate.auth.support.RefreshTokenFactory.REFRESH_TOKEN_COOKIE_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class RefreshTokenFactoryTest {
+
+    @InjectMocks
+    private RefreshTokenFactory refreshTokenFactory;
+
+    @Test
+    @DisplayName("리프레시 토큰 쿠키 생성 테스트")
+    void createRefreshToken() {
+        String refreshToken = "RefreshToken";
+
+        ResponseCookie result = refreshTokenFactory.fromRefreshToken(refreshToken);
+
+        assertThat(result.getName()).isEqualTo(REFRESH_TOKEN_COOKIE_NAME);
+        assertThat(result.getValue()).isEqualTo(refreshToken);
+        assertThat(result.isHttpOnly()).isTrue();
+        assertThat(result.getSameSite()).isEqualTo("Lax");
+        assertThat(result.isSecure()).isTrue();
+    }
+}
