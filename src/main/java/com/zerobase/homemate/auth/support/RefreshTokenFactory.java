bb35867a7171d085fa919eef86a66e3aefdbf8a0
escha-k <bbs7380@gmail.com>
2025-12-27 18:21:24 +0900

    feat: RefreshTokenFactory 구현

---
+++ src/main/java/com/zerobase/homemate/auth/support/RefreshTokenFactory.java
@@ -0,0 +1,23 @@
+package com.zerobase.homemate.auth.support;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RefreshTokenFactory {
+
+    @Value("${auth.jwt.refresh-exp-seconds}")
+    private long refreshExp;
+
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+
+    public ResponseCookie fromRefreshToken(String refreshToken) {
+        return ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, refreshToken)
+                .httpOnly(true)
+                .sameSite("Lax")
+                .secure(true)
+                .maxAge(refreshExp)
+                .build();
+    }
+}
