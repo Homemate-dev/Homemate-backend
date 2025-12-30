e82e1e94d42b5980df7d2cf0df137c6565ea64ad
hj <kwonfelix93@gmail.com>
2025-12-30 14:20:17 +0900

    [feat] 집안일 필터별 리스트 조회 기능 추가

--- src/main/java/com/zerobase/homemate/repository/ChoreRepository.java
+++ src/main/java/com/zerobase/homemate/repository/ChoreRepository.java
@@ -1,6 +1,8 @@
 package com.zerobase.homemate.repository;
 
 import com.zerobase.homemate.entity.Chore;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface ChoreRepository extends JpaRepository<Chore, Long> {
 
+    List<Chore> findByUserIdAndIsDeletedIsFalse(Long userId, Sort sort);
 
     Optional<Chore> findByUserIdAndTitle(Long id, String title);
 }
