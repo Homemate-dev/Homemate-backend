02107427c4ecdaa1a22cacdac5a623d7e1948cbf
hj <kwonfelix93@gmail.com>
2025-12-30 14:24:56 +0900

    [feat] 미사용 메서드 삭제

--- src/main/java/com/zerobase/homemate/repository/ChoreRepository.java
+++ src/main/java/com/zerobase/homemate/repository/ChoreRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
 public interface ChoreRepository extends JpaRepository<Chore, Long> {
 
