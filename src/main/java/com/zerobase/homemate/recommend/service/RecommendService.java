50be8d0194e70050f53ad544d11086953ff64e13
Delcastin <lejesu96@naver.com>
2026-02-24 21:20:01 +0900

    refactor / monthly_categories_date
    - 월간 카테고리 조회하기 날짜 -> 카테고리 ID, 이름 조회
    
    -> 날짜만 알면 카테고리까지 조회할 수 있도록 날짜 -> 카테고리들을 추가하였습니다.

--- src/main/java/com/zerobase/homemate/recommend/service/RecommendService.java
+++ src/main/java/com/zerobase/homemate/recommend/service/RecommendService.java
@@ -1,11 +1,15 @@
 package com.zerobase.homemate.recommend.service;
 
 
+import com.zerobase.homemate.recommend.dto.MonthlyRecommendResponse;
 import com.zerobase.homemate.recommend.dto.SpaceChoreResponse;
+import com.zerobase.homemate.repository.CategoriesRepository;
 import com.zerobase.homemate.repository.SpaceChoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.YearMonth;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -13,8 +17,20 @@ import java.util.List;
 public class RecommendService {
 
     private final SpaceChoreRepository spaceChoreRepository;
+    private final CategoriesRepository categoriesRepository;
 
     public List<SpaceChoreResponse> getRandomChores(){
         return spaceChoreRepository.findRandomChores();
+    }
+
+    public List<MonthlyRecommendResponse> getMonthlyCategories(){
+
+        YearMonth now = YearMonth.now(ZoneId.of("Asia/Seoul"));
+        String targetMonth = now.toString();
+
+        return categoriesRepository.findActiveMonthlyByTargetMonth(targetMonth)
+                .stream()
+                .map(MonthlyRecommendResponse::from)
+                .toList();
     }
 }
